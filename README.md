54bf210ac72397ad61f1c2af18dfe5c832ec4d1c
Ngoc Vo <ngocduyvo12@yahoo.com>
2019-10-03 20:39:53 -0600

    added do-what-it-says command and verified that it works. Also updated readme.ms

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # liri-bot
+
+This app is designed to use 3 different api to look up specific information. By entering different command after `node liri.js` the user can look up different things.
+
+![video of app working](https://youtu.be/gCqkXm71aws)
+
+The app can be run through git bash:
+1. Use git bash to navigate to the root folder
+2. open the liri.js with node: node liri.js
+3. enter one of the three specified search term: node liri.js <search>
+search Terms: 
+* spotify-this-song
+* concert-this
+* movie-this
+* do-what-it-says
+4. Enter what you want to look up: node liri.js <search> <something>
+5. Enjoy!
+
+The app is made using three different apis. 
+1. spotify-api
+2. omdp api
+3. band in town api
+
+TO make sure everyone can use this app, I first have to initialize package.json and install my npm there. 
+
+The NPM used in this app are:
+1. node-spotify-api
+2. axios
+3. moment
+4. dotenv
+5. fs (built-in)
+
+To use this app, spotify-key and spotify-secret are required. This can be obtained by going to spotify developer page and sign up. After you have obtained the keys and secret:
+Replace the content of **var spotify = new Spotify(keys.spotify)** with
+ **var spotify = new Spotify({
+  id: <your spotify client id>,
+  secret: <your spotify client secret>
+});** replace <your spotify client id> with your spotify-id and replace <your spotify client secret> with your secret. No quotes!
+
+After I initialize all the required npm, I have to assign the input from **process.argv[2]** to a search variable and **process.argv[3]** to a term variable. Although in the case of term, *slice()* and *join()* was use to get more dynamic input. 
+
+A boolean condition was set-up to make sure the user will always enter a valid input. If there is an input call function **callCommand()**.
+
+Inside function **callCommand()**. Different search terms are being evaluated using switch. For each of the valid search term mentioned above, the app will get the term and reach out their respective api and get the desired information.
+
+If there is no input or if the input is incorrect. The app will let the user know that the input is invalid.
+
+This app is a personal project I made for coding bootcamp.
--- liri.js
+++ liri.js
@@ -16,43 +16,99 @@ var search = process.argv[2]; //search type, no need for slice since it is joine
 var term = process.argv.slice(3).join("+") //search term. slice and join to make it readable
 
 
-//use switch case to switch between cases of search
-switch (search) {
-    //use spotify npm for case spotify-this-song
-    case "spotify-this-song":
-        //set default term when no term entered
-        if (!term) {
-            term = "the sign ace of base"
-        }
-        //search using npm
-        spotify.search({ type: 'track', query: term, limit: 1 })
-            .then(function (response) {
-                var divider = "----------------------- \n"
-                console.log(`${divider}Song name: ${response.tracks.items[0].name} 
-              \nArtist: ${response.tracks.items[0].album.artists[0].name} 
-              \nAlbum: ${response.tracks.items[0].album.name}
-              \nSong link: ${response.tracks.items[0].href} 
-              `);
 
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
-        break;
+if (!search) {
+    console.log("Please enter a valid search term")
+} else (
+    callCommand()
+)
 
-    //use axios with bands in town api for cocert-this case
-    case "concert-this":
-        var divider = `\n------------------------\n`
-        var concertURL = `https://rest.bandsintown.com/artists/${term}/events?app_id=codingbootcamp`
-        axios.get(concertURL).then(function (response) {
+function callCommand() {
 
-            //convert datetime into MM/DD/YYYY format with moment:
-            for (var i = 0; i < response.data.length; i++) {
-                var dateTime = response.data[i].datetime;
-                var dateFormat = "YYYY-MM-DDTHH:mm:ssZ"
-                var dateConverted = moment(dateTime, dateFormat).format("MM/DD/YYYY")
-
-                console.log(`Name of Venue: ${response.data[i].venue.name} \nVenue Location: ${response.data[i].venue.city}, ${response.data[i].venue.region}, ${response.data[i].venue.country} \nDate of Event: ${dateConverted} ${divider}`)
+    //use switch case to switch between cases of search
+    switch (search) {
+        //use spotify npm for case spotify-this-song
+        case "spotify-this-song":
+            //set default term when no term entered
+            if (!term) {
+                term = "the sign ace of base"
             }
-        })
+            //search using npm
+            spotify.search({ type: 'track', query: term, limit: 1 })
+                .then(function (response) {
+                    var divider = "----------------------- \n"
+                    console.log(`${divider}Song name: ${response.tracks.items[0].name} 
+                \nArtist: ${response.tracks.items[0].album.artists[0].name} 
+                \nAlbum: ${response.tracks.items[0].album.name}
+                \nSong link: ${response.tracks.items[0].href} 
+                `);
+
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+            break;
+
+        //use axios with bands in town api for cocert-this case
+        case "concert-this":
+            var divider = `\n------------------------\n`
+            var concertURL = `https://rest.bandsintown.com/artists/${term}/events?app_id=codingbootcamp`
+            axios.get(concertURL).then(function (response) {
+
+                for (var i = 0; i < response.data.length; i++) {
+
+                    //convert datetime into MM/DD/YYYY format with moment:
+                    var dateTime = response.data[i].datetime;
+                    var dateFormat = "YYYY-MM-DDTHH:mm:ssZ"
+                    var dateConverted = moment(dateTime, dateFormat).format("MM/DD/YYYY")
+                    //console log desired information
+                    console.log(`${divider} Name of Venue: ${response.data[i].venue.name}
+                    \nVenue Location: ${response.data[i].venue.city}, ${response.data[i].venue.region}, ${response.data[i].venue.country}
+                    \nDate of Event: ${dateConverted} `)
+                    // console.log(response.data[i].venue.name)
+                }
+            })
+            break;
+        case "movie-this":
+            if (!term) {
+                term = "Mr. Nobody"
+            }
+
+            var divider = `\n------------------------\n`
+            var movieURL = `http://www.omdbapi.com/?t=${term}&y=&plot=short&apikey=trilogy`
+            //set default if no term entered:
+            //console log desired info
+            axios.get(movieURL).then(function (response) {
+                console.log(`${divider} Title: ${response.data.Title}
+                \nYear: ${response.data.Year}
+                \nIMDB: ${response.data.imdbRating} 
+                \nRotten Tomatoes: ${response.data.Ratings[1].Value}
+                \nCountry: ${response.data.Country}
+                \nLanguage: ${response.data.Language}
+                \nPlot: ${response.data.Plot}
+                \nActors: ${response.data.Actors}`)
+            })
+            break;
+
+        case "do-what-it-says":
+            fs.readFile("random.txt", "utf8", function (error, data) {
+                //return error
+                if (error) {
+                    return console.log(error);
+                }
+                //Split string in random.txt into array
+                var dataArr = data.split(",")
+                //set search equal to first value in dataArr
+                search = dataArr[0]
+                //Set term equal to second value in dataArr
+                term = dataArr[1]
+                //call function again with new inputs
+                callCommand()
+            })
+            break;
+        default: 
+        console.log("Please enter a valid input")
+
+    }
 }
+
